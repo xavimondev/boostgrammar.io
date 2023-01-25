eae3ccfbfafb911a711789896b41be7529621c4d
Xavier Alfaro <nxb.developer@gmail.com>
2023-01-25 17:33:48 -0500

    add prettier and linter configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['standard', 'plugin:astro/recommended'],
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {}
+    }
+  ]
+}
