22089beb2d20c92dd06795fe2faf7d7694d6d9d7
Xavier Alfaro <nxb.developer@gmail.com>
2023-01-26 17:58:35 -0500

    add configuration for supabase

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_SUPABASE_URL: string
+  readonly PUBLIC_SUPABASE_ANON_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
