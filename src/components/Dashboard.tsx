4ffe237ea8eed28c6e82bf19b6c4202bfa4953ae
Xavier Alfaro <nxb.developer@gmail.com>
2023-01-26 23:15:19 -0500

    add design dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,3 +1,24 @@
+import { useState, useEffect } from 'preact/hooks'
+import { supabase } from '../lib/database'
+
 export function Dashboard() {
-  return <h1>Dashboard</h1>
+  const [session, setSession] = useState(null)
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+      console.log(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+      console.log(session)
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>Eyyy hola</h1>
+      {!session ? <span>User not logged</span> : <span>User logged</span>}
+    </>
+  )
 }
