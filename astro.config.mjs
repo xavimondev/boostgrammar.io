8a4c06199b1547899a45e4eb487182e72c15894e
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-03 13:30:50 -0500

    remove endpoints

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,6 @@ import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 
 // https://astro.build/config
-import cloudflare from '@astrojs/cloudflare'
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind(), preact()],
-  adapter: cloudflare()
+  integrations: [tailwind(), preact()]
 })
