c5425cc59c9460346bb1110a573130ad422370a6
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-02 22:32:02 -0500

    add cloudfare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
-import netlify from '@astrojs/netlify/functions'
+
+// https://astro.build/config
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), preact()],
-  adapter: netlify()
+  adapter: cloudflare()
 })
