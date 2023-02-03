b1ee241b50097ec19edb1b446caea732f1ff7d77
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-02 19:18:23 -0500

    update notify function to be more flexible

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,11 +1,38 @@
 import { toast } from 'wc-toast'
 
-export const notify = (text: string) => {
-  toast.success(text, {
+type TypeNotify = 'success' | 'promise'
+// const theme = {
+//   type: 'custom',
+//   style: { background: 'white', color: 'black' }
+// }
+const successNotify = (textSuccess: string) => {
+  toast.success(textSuccess, {
     duration: 2000,
     theme: {
       type: 'custom',
       style: { background: 'white', color: 'black' }
     }
   })
+}
+
+const promiseNotify = (textSuccess: string, promise: Promise<any>) => {
+  toast.promise(
+    promise,
+    {
+      loading: 'Saving document...',
+      success: textSuccess,
+      error: 'An error has ocurred'
+    },
+    {
+      theme: {
+        type: 'custom',
+        style: { background: 'white', color: 'black' }
+      }
+    }
+  )
+}
+
+export const notify = (typeNotify: TypeNotify, textSuccess: string, promise?: Promise<any>) => {
+  if (typeNotify === 'promise') promiseNotify(textSuccess, promise)
+  else if (typeNotify === 'success') successNotify(textSuccess)
 }
