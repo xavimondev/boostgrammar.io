836a55bd91eec47bbe92ad5f4ba8d77328608a20
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-03 20:30:13 -0500

    add fastify server

---
+++ api/index.js
@@ -0,0 +1,79 @@
+import Fastify from 'fastify'
+import fastifyEnv from '@fastify/env'
+import cors from '@fastify/cors'
+import path from 'node:path'
+import { RAPID_API_ENDPOINT } from './constants.js'
+
+const server = Fastify({ logger: true })
+await server.register(cors, {
+  methods: ['GET', 'POST']
+})
+
+const schema = {
+  type: 'object',
+  required: ['RAPID_API_KEY'],
+  properties: {
+    RAPID_API_KEY: {
+      type: 'string',
+      default: ''
+    }
+  }
+}
+const ENV_PATH = path.join(process.cwd(), 'api/.env')
+const optionsConf = {
+  schema,
+  dotenv: {
+    path: ENV_PATH
+  }
+}
+
+// Run the server!
+const start = async () => {
+  try {
+    await server.register(fastifyEnv, optionsConf)
+    await server.ready()
+    await server.listen({ port: 3001 })
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+
+// Declare a route
+server.route({
+  method: 'GET',
+  url: '/dictionary',
+  schema: {
+    querystring: {
+      word: {
+        type: 'string'
+      }
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          data: {
+            type: 'array'
+          }
+        }
+      }
+    }
+  },
+  handler: async (req, reply) => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': 'thesaurus-by-api-ninjas.p.rapidapi.com'
+      }
+    }
+    const { word } = req.query
+    const requestFetch = await fetch(`${RAPID_API_ENDPOINT}${word}`, options)
+    const response = await requestFetch.json()
+    const data = response.synonyms
+    return { data }
+  }
+})
+
+start()
