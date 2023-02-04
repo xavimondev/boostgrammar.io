69aeaff34f912bbb1a489037534200e4a62d67eb
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-04 17:59:37 -0500

    update styles on grammarfix component

--- src/components/GrammarFix.tsx
+++ src/components/GrammarFix.tsx
@@ -21,90 +21,112 @@ export function GrammarFix({ isLoading, totalWords }) {
   const hasResults = outputValue.length > 0
   return (
     <>
-      <div class='grid grid-cols-1 md:grid-cols-2 h-full'>
-        <div class='w-full flex flex-col'>
+      <div class='flex flex-col md:flex-row gap-8 sm:gap-4 w-full h-full'>
+        <div class='flex flex-col h-full w-full'>
           <div class='mb-10'>
             <input
               id='documentTitle'
               type='text'
-              class='bg-[#0d1117] font-bold sm:text-lg border-none focus:outline-none text-white w-full'
+              class='bg-[#0d1117] font-bold text-base border-none focus:outline-none text-white w-full'
               placeholder='Change document name'
               autofocus
             />
           </div>
-          <div class='h-full'>
-            <UserInput
-              updateOutputValue={setOutputValue}
-              setLoading={setLoading}
-              totalWords={totalWords}
-            />
-          </div>
-        </div>
-        <aside class='w-full'>
-          <div class='flex flex-col h-full gap-4'>
-            <div class='flex flex-row justify-between mb-4'>
-              <span class='font-bold text-white text-base sm:text-lg'>Suggestions</span>
-              <button
-                disabled={!hasResults}
-                onClick={async () => {
-                  const text = transformStringArrayToString(outputValue)
-                  await copyTextToClipboard(text)
-                  notify('success', 'Text copied')
-                }}
-              >
-                <CopyToClipboardIc
-                  class={`h-8 w-8 ${hasResults ? 'text-white' : 'text-gray-500'} items-center`}
-                />
-              </button>
+          <div class='flex flex-col gap-2 h-full w-full'>
+            <div class='h-1/2'>
+              <UserInput
+                updateOutputValue={setOutputValue}
+                setLoading={setLoading}
+                totalWords={totalWords}
+              />
             </div>
-            <div class='h-3/5 overflow-y-auto'>
-              {hasResults ? (
-                <div class='flex gap-1 flex-wrap'>
-                  {outputValue.map((word) => (
-                    <WordOutput
-                      word={word}
-                      synonyms={synonyms}
-                      wordSelected={wordSelected}
-                      isLoadingSynonyms={isLoadingSynonyms}
-                    />
-                  ))}
-                </div>
-              ) : (
-                <WaitingData />
-              )}
-            </div>
-            <div class='h-2/5 flex flex-col gap-4'>
-              <span class='font-bold text-white text-base sm:text-lg'>Synonyms</span>
-              <div class='flex flex-wrap gap-3'>
-                {/* TODO: Add component synonyms */}
-                {synonyms.value.length > 0 ? (
-                  <>
-                    {synonyms.value.map((syn: string) => (
-                      <button class='text-sm sm:text-base py-1 sm:py-1.5 px-6 sm:px-8 text-white rounded-full font-semibold transition ease-in-out delay-75 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-green-500 duration-300'>
-                        {syn}
-                      </button>
+            <div class='flex flex-col gap-2 h-1/2 w-full'>
+              <div class='flex flex-row justify-between w-full'>
+                <span class='font-bold text-white text-base sm:text-lg'>Output</span>
+                <button
+                  disabled={!hasResults}
+                  onClick={async () => {
+                    const text = transformStringArrayToString(outputValue)
+                    await copyTextToClipboard(text)
+                    notify('success', 'Text copied')
+                  }}
+                >
+                  <CopyToClipboardIc
+                    class={`h-8 w-8 ${hasResults ? 'text-white' : 'text-gray-500'} items-center`}
+                  />
+                </button>
+              </div>
+              <div class='h-full flex items-center w-full'>
+                {hasResults ? (
+                  <div class='flex gap-1 flex-wrap overflow-y-auto'>
+                    {outputValue.map((word) => (
+                      <WordOutput
+                        word={word}
+                        synonyms={synonyms}
+                        wordSelected={wordSelected}
+                        isLoadingSynonyms={isLoadingSynonyms}
+                      />
                     ))}
-                  </>
-                ) : null}
-                {/* TODO: Add component while is rendering */}
-                {!isLoadingSynonyms.value &&
-                synonyms.value.length === 0 &&
-                wordSelected.value !== '' ? (
-                  <p class='text-gray-400 text-sm sm:text-base'>
-                    No synonyms for <span class='font-bold'>to</span> found
-                  </p>
-                ) : null}
-
-                {wordSelected.value === '' && (
-                  <span class='text-gray-400 text-base sm:text-lg'>
-                    Select a word to see its <span class='font-bold'>synonyms</span>
-                  </span>
-                )}
-                {isLoadingSynonyms.value && wordSelected.value !== '' && (
-                  <p class='text-base sm:text-lg font-bold'>Loading...</p>
+                  </div>
+                ) : (
+                  <WaitingData />
                 )}
               </div>
             </div>
+          </div>
+        </div>
+        <aside class='w-full h-full hidden md:block'>
+          <div class='flex flex-col h-full gap-4'>
+            <span class='font-bold text-white text-base sm:text-lg'>Results</span>
+            {/* Results component */}
+            <div class='flex flex-wrap gap-8 justify-center'>
+              <div class='flex flex-col gap-0.5 items-center'>
+                <span class='font-bold text-base sm:text-4xl text-green-400'>WORDS</span>
+                <span class='font-semibold text-sm sm:text-5xl text-green-400'>
+                  {totalWords.value}
+                </span>
+              </div>
+              <div class='flex flex-col gap-0.5 items-center'>
+                <span class='font-bold text-base sm:text-4xl text-red-400'>MISTAKES</span>
+                <span class='font-semibold text-sm sm:text-5xl text-red-400'>
+                  {totalWords.value}
+                </span>
+              </div>
+            </div>
+            {/* Text animation typing saying something like: Here are your mistakes */}
+            {/* I need to show words that are not in ouput and highlight them */}
+            {/* If user does not have mistakes, app will show an animation with check icon saying everything is ok */}
+            {/* TODO: Add component synonyms */}
+            {/* {synonyms.value.length > 0 ? (
+                <>
+                  {synonyms.value.map((syn: string) => (
+                    <button class='text-sm sm:text-base py-1 sm:py-1.5 px-6 sm:px-8 text-white rounded-full font-semibold transition ease-in-out delay-75 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-green-500 duration-300'>
+                      {syn}
+                    </button>
+                  ))}
+                </>
+              ) : null} */}
+            {/* Validation for synonyms */}
+            {/* {!isLoadingSynonyms.value &&
+              synonyms.value.length === 0 &&
+              wordSelected.value !== '' ? (
+                <p class='text-gray-400 text-sm sm:text-base'>
+                  No synonyms for <span class='font-bold'>to</span> found
+                </p>
+              ) : null} */}
+
+            {/* 
+              SYNONYMS VALIDATION
+              {wordSelected.value === '' && (
+                <span class='text-gray-400 text-base sm:text-lg'>
+                  Select a word to see its <span class='font-bold'>synonyms</span>
+                </span>
+              )} */}
+            {/* 
+              Loading synonyms
+              {isLoadingSynonyms.value && wordSelected.value !== '' && (
+                <p class='text-base sm:text-lg font-bold'>Loading...</p>
+              )} */}
           </div>
         </aside>
       </div>
