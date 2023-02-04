809ec4ad43c187654b3b595b5f6f164ed9b943b0
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-04 17:53:35 -0500

    add more icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1298,3 +1298,78 @@ export const CopyToClipboardIc = (props) => {
     </svg>
   )
 }
+
+export function LoadingIc(props) {
+  return (
+    <svg
+      {...props}
+      id='L6'
+      xmlns='http://www.w3.org/2000/svg'
+      x='0px'
+      y='0px'
+      viewBox='0 0 100 100'
+      enable-background='new 0 0 100 100'
+    >
+      <rect fill='none' stroke='#fff' stroke-width='4' x='25' y='25' width='50' height='50'>
+        <animateTransform
+          attributeName='transform'
+          dur='0.5s'
+          from='0 50 50'
+          to='180 50 50'
+          type='rotate'
+          id='strokeBox'
+          attributeType='XML'
+          begin='rectBox.end'
+        />
+      </rect>
+      <rect x='27' y='27' fill='#fff' width='46' height='50'>
+        <animate
+          attributeName='height'
+          dur='1.3s'
+          attributeType='XML'
+          from='50'
+          to='0'
+          id='rectBox'
+          fill='freeze'
+          begin='0s;strokeBox.end'
+        />
+      </rect>
+    </svg>
+  )
+}
+
+export function ThinkIc(props) {
+  return (
+    <svg viewBox='0 0 72 72' {...props}>
+      <g fill='#FCEA2B'>
+        <circle cx='36' cy='36.2' r='24'></circle>
+        <path d='M19.1 62.3c-.3-1.2-.4-2.4-.3-3.6.2-1.1 2.3-6.5 2.7-7.3s1.4-4.5 2.4-4.8c1-.3 1.2.1 1.1.7s-.6 7.7-.6 7.7l9.4-2.8s7.7-1.9 7.9-1c2.1 2-5.4 4.5-7.5 4.4 0 0 1.1.6.9 1.3s-.8 1.2-1 1.2.5.5.6 1.4c.9 1.8-2.8 2.3-2.8 2.3 1.3-.2 3.3 2.1.1 2.7-1 .3-4.8 1.4-4.8 1.4s-3.3 1.2-5.2-.1c-1.2-.8-2.3-2-2.9-3.5z'></path>
+      </g>
+      <path
+        fill='none'
+        stroke='#000'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='2'
+        d='M28.9 44.5c2.4-.7 11.9.5 14.1 2.1M22.4 22.5c1.3-.9 2.8-1.3 4.4-1.1 1.6 0 3.1.6 4.2 1.7M51.1 26.5c-2.7-.7-5.5-.8-8.2-.3'
+      ></path>
+      <path
+        fill='none'
+        stroke='#000'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='2'
+        d='M42 58.4c12.3-3.3 19.5-15.9 16.2-28.2S42.3 10.7 30 14c-10 2.7-17 11.8-17 22.2 0 4.6 1.4 9.2 4 13'
+      ></path>
+      <path d='M31 30.7c0 1.7-1.3 3-3 3s-3-1.3-3-3 1.3-3 3-3 3 1.3 3 3M48 33.7c0 1.7-1.3 3-3 3s-3-1.3-3-3 1.3-3 3-3 3 1.3 3 3'></path>
+      <path
+        fill='none'
+        stroke='#000'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='2'
+        d='M19.7 56.7c-1 1.6-1.2 3.7-.5 5.5 1.1 3.3 4 5.2 7.2 4.2l4.9-1.4 1-.3c.8-.2 1.3-1.1 1.1-1.8h0c-.2-.5-.6-.9-1.2-1l1.2-.3c.8-.2 1.3-1.1 1.1-1.8-.2-.5-.6-1-1.2-1.1l1.2-.4c.8-.2 1.3-1.1 1.1-1.8-.1-.5-.5-.9-1-1l6.4-1.9c.8-.2 1.3-1.1 1.1-1.8s-1.1-1.3-1.8-1.1h0l-10.2 3-5.1 1.5.6-7.8c.1-.9-1.5-1.2-2.1.2l-3.8 9.1z'
+      ></path>
+    </svg>
+  )
+}
