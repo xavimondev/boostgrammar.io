638a1ea5f3376c337706a1bc4a9688e93ff296c6
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-03 23:13:45 -0500

    add cohere api on server

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import Fastify from 'fastify'
 import fastifyEnv from '@fastify/env'
 import cors from '@fastify/cors'
 import path from 'node:path'
-import { RAPID_API_ENDPOINT } from './constants.js'
+import { COHERE_API_GENERATE_URL, PROMPT_EXAMPLES, RAPID_API_ENDPOINT } from './constants.js'
 
 const server = Fastify({ logger: true })
 await server.register(cors, {
@@ -11,7 +11,7 @@ await server.register(cors, {
 
 const schema = {
   type: 'object',
-  required: ['RAPID_API_KEY', 'PORT'],
+  required: ['RAPID_API_KEY', 'PORT', 'COHERE_API_KEY'],
   properties: {
     RAPID_API_KEY: {
       type: 'string',
@@ -20,6 +20,10 @@ const schema = {
     PORT: {
       type: 'number',
       default: 3000
+    },
+    COHERE_API_KEY: {
+      type: 'string',
+      default: ''
     }
   }
 }
@@ -86,6 +90,58 @@ server.route({
     const response = await requestFetch.json()
     const data = response.synonyms
     return { data }
+  }
+})
+
+server.route({
+  method: 'POST',
+  url: '/grammar',
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          output: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  },
+  handler: async (req, reply) => {
+    const body = req.body
+    const { input } = JSON.parse(body)
+    console.log(input)
+    const data = {
+      model: 'xlarge',
+      prompt: `${PROMPT_EXAMPLES}
+      Incorrect sample:${input}
+      Correct sample:`,
+      max_tokens: 400,
+      temperature: 0, // same response
+      k: 0,
+      p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop_sequences: ['--'],
+      return_likelihoods: 'NONE'
+    }
+
+    const requestCohere = await fetch(COHERE_API_GENERATE_URL, {
+      method: 'POST',
+      headers: {
+        Authorization: `BEARER ${process.env.COHERE_API_KEY}`,
+        'Content-Type': 'application/json',
+        'Cohere-Version': '2022-12-06'
+      },
+      body: JSON.stringify(data)
+    })
+    const response = await requestCohere.json()
+    const { text } = response.generations[0]
+    const textOutput = text.replace('--', '').replaceAll('"', '').trim()
+    return {
+      output: textOutput
+    }
   }
 })
 
