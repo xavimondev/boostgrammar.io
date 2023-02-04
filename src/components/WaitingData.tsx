04423d508423ffcc57a07fb1a2fe77248904a56c
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-04 17:58:18 -0500

    update waitingdata component with new icon

--- src/components/WaitingData.tsx
+++ src/components/WaitingData.tsx
@@ -1,13 +1,11 @@
-import { SearchingIc } from './Icons'
+import { ThinkIc } from './Icons'
 
 export function WaitingData() {
   return (
-    <div class='flex flex-col items-center justify-center'>
-      <SearchingIc />
-      <div class='mt-0 sm:mt-1 flex flex-col gap-2 sm:gap-6 text-center'>
-        <span class='font-bold text-xl sm:text-2xl text-white'>Nothing to check yet</span>
-        <p class='text-base sm:text-lg text-gray-400'>Start writing to generate feedback.</p>
-      </div>
+    <div class='flex flex-col items-center gap-6 h-full w-full mt-6'>
+      <ThinkIc class='inline-block w-24 h-24 sm:w-28 sm:h-28' />
+      <span class='font-bold text-base sm:text-xl text-white'>Nothing to check yet</span>
+      <p class='text-sm sm:text-base text-gray-400'>Start writing to generate feedback.</p>
     </div>
   )
 }
