5855d068f8460c801d2542f292ccb567938391eb
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-04 18:51:35 -0500

    add new animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,12 +4,19 @@ module.exports = {
 	theme: {
 		extend: {
 			animation: {
-				'bar-infinite': 'bar-infinite 3s linear infinite'
+				'bar-infinite': 'bar-infinite 3s linear infinite',
+				'line-up': 'anim-lineUp 2s ease-out 1'
 			},
 			keyframes: {
 				'bar-infinite': {
 					'0%': { left: '-100%' },
 					'100%': { left: '100%' }
+				},
+				'anim-lineUp': {
+					'0%': { opacity: '0', transform: 'translateY(80%)' },
+					'20%': { opacity: '0' },
+					'50%': { opacity: '1', transform: 'translateY(0%)' },
+					'100%': { opacity: '1', transform: 'translateY(0%)' }
 				}
 			}
 		}
