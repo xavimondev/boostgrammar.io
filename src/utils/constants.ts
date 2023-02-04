5345c2229370464baaa27bc8d282ced7f512e7f7
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-03 23:30:20 -0500

    fix client constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
-const SERVER_URL = 'https://boostgrammar.up.railway.app'
-const LOCAL_URL = 'http://localhost:3001'
+const REMOTE_SERVER_URL = 'https://boostgrammar.up.railway.app'
+const LOCAL_SERVER_URL = 'http://localhost:3001'
+const LOCAL_CLIENT_URL = 'http://localhost:3000'
 const isDevelopment = import.meta.env.MODE === 'development'
 
-export const SERVER = isDevelopment ? LOCAL_URL : SERVER_URL
-export const DASHBOARD_URL = isDevelopment ? `${LOCAL_URL}/dashboard` : `${SERVER_URL}/dashboard`
+export const SERVER = isDevelopment ? LOCAL_SERVER_URL : REMOTE_SERVER_URL
+export const DASHBOARD_URL = isDevelopment
+  ? `${LOCAL_CLIENT_URL}/dashboard`
+  : `${REMOTE_SERVER_URL}/dashboard`
