87522d87dc1bc06bd6112582a7d88fcf15037dcc
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-05 12:57:22 -0500

    update component receives functions as props

--- src/components/WordOutput.tsx
+++ src/components/WordOutput.tsx
@@ -1,24 +1,38 @@
 import { getSynonyms } from '@services/synonyms'
+import { PopoverCoordinates } from 'src/types'
 
-// TODO: Add types
 type WordOutputProps = {
   word: string
   synonyms: any
-  wordSelected: any
-  isLoadingSynonyms: any
+  setWordSelected: (word: string) => void
+  setIsLoadingSynonyms: VoidFunction
+  setPopoverCoordinates: (coordinates: PopoverCoordinates) => void
+  openPopover: VoidFunction
 }
 
-export function WordOutput({ word, synonyms, wordSelected, isLoadingSynonyms }: WordOutputProps) {
+export function WordOutput({
+  word,
+  synonyms,
+  setWordSelected,
+  setIsLoadingSynonyms,
+  setPopoverCoordinates,
+  openPopover
+}: WordOutputProps) {
   return (
     <span
       class='text-base sm:text-lg text-white'
       id='grammarOutput'
-      onDblClick={async () => {
-        wordSelected.value = word
-        isLoadingSynonyms.value = true
+      onDblClick={async (event) => {
+        setPopoverCoordinates({
+          left: event.currentTarget.offsetLeft,
+          top: event.currentTarget.offsetTop + 28
+        })
+        openPopover()
+        setWordSelected(word)
+        setIsLoadingSynonyms()
         const data = await getSynonyms(word)
         synonyms.value = data
-        isLoadingSynonyms.value = false
+        setIsLoadingSynonyms()
       }}
     >
       {word}
