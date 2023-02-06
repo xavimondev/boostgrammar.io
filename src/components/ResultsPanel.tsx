543459e0400359c1cd82566aa909a450db3f0f31
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-06 17:36:13 -0500

    add toolbar to see results on mobile

--- src/components/ResultsPanel.tsx
+++ src/components/ResultsPanel.tsx
@@ -8,3 +8,14 @@ export function ResultsPanel({ children }) {
     </aside>
   )
 }
+
+export function ResultsPanelToolbar(props) {
+  return (
+    <button
+      class='absolute left-0 right-0 bottom-20 block md:hidden rounded-full bg-gradient-to-r from-[#11ccda] to-[#4490f2] w-1/2 m-auto px-2 py-2.5 text-center animate-bounce'
+      {...props}
+    >
+      <span class='text-white font-semibold'>See Results</span>
+    </button>
+  )
+}
