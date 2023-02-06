4a81caba5eaab89e7905df9de6554e242f4192d8
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-05 20:42:29 -0500

    add service to fetch grammatical errors

--- src/services/grammar.ts
+++ src/services/grammar.ts
@@ -10,3 +10,17 @@ export const getCorrectionFromInput = async (input: string) => {
   const data = await response.json()
   return data.output
 }
+
+export const getGrammaticalMistakesFromText = async (text: string) => {
+  const request = await fetch(`${SERVER}/checking`, {
+    method: 'POST',
+    body: JSON.stringify({
+      text
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  const response = await request.json()
+  return response.result
+}
