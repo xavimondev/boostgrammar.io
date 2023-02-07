ff9f4b09a873261c7c1e8cfe11ad0702b6554560
Xavier Alfaro <nxb.developer@gmail.com>
2023-02-07 09:31:24 -0500

    update illustration on empty state

--- src/components/NoDataFound.tsx
+++ src/components/NoDataFound.tsx
@@ -1,11 +1,13 @@
-import { QuestionIc } from './Icons'
-
 export function NoDataFound() {
   return (
     <div class='flex flex-col items-center gap-6 h-full w-full mt-36 justify-center text-center'>
-      <QuestionIc class='text-sky-400 w-24 h-24 sm:w-28 sm:h-28' />
-      <span class='font-semibold text-base sm:text-2xl text-white/90'>
-        You haven't created documents yet.
+      <img
+        src='/nodata.svg'
+        alt='There are not documents yet Illustration'
+        class='w-52 h-52 sm:w-80 sm:h-80'
+      />
+      <span class='font-semibold text-base sm:text-2xl text-white max-w-2xl'>
+        You haven't created documents yet. You do not need to be authenticated to create a document.
       </span>
     </div>
   )
